c7be33eea42f70aef01d0c1d48c72ef3bde3ed29
fran <franciscogaray@live.com.ar>
2022-10-30 14:41:57 -0300

    una cosa mas de lo anterior

--- app/controllers/Admins/sessions_controller.rb
+++ app/controllers/Admins/sessions_controller.rb
@@ -2,17 +2,16 @@
 
 class Admins::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
-  before_action :check_if_logged_in, only: [:new, :create]
 
   # GET /resource/sign_in
   def new
-    check_if_logged_in
+    # check_if_logged_in
     super
   end
 
   # POST /resource/sign_in
   def create
-    check_if_logged_in
+    # check_if_logged_in
     super
   end
 
@@ -33,18 +32,6 @@ class Admins::SessionsController < Devise::SessionsController
   end
 
   def after_log_ing_path_for(resource)
-    :admins_root
-  end
-
-  def after_sign_up_path_for(resource)
-    :admins_root
-  end
-
-  def check_if_logged_in
-    redirect_to :admins_root if admin_signed_in?
-  end
-
-  def admins_root_path
     :admins_root
   end
 end
