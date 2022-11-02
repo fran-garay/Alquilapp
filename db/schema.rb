b2184c77957ae1c822ce24db8727469bc1faa426
Laureano Bruno <laureanobruno@hotmail.com>
2022-11-02 01:32:52 -0300

    Tabla, modelo y controlados de billetera

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_185254) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_042225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_185254) do
     t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.float "saldo"
+    t.float "ultimo_gasto"
+    t.float "ultima_carga"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
