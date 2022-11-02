506664aed20371c490ae90cb28e198a45908c345
Laureano Bruno <laureanobruno@hotmail.com>
2022-11-02 13:20:21 -0300

    Pruebas con billetera

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,12 @@
 class WalletsController < ApplicationController
+
+    def show
+        @saldo  = Wallet.all
+    end
+
+    def cargar_saldo
+        w = Wallet.find_by(user_id: :user_id)
+        w.saldo = :monto + w.saldo
+    end
+
 end
