d6965804990081989f984970ac88f05ce8d679ac
Laureano Bruno <laureanobruno@hotmail.com>
2022-11-03 21:50:43 -0300

    Prueba

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,12 +1,18 @@
 class WalletsController < ApplicationController
 
-    def show
-        @saldo  = Wallet.all
+    def mostrar_wallet
+        @wallet = Wallet.find_by(user_id: :user_id)
     end
 
-    def cargar_saldo
-        w = Wallet.find_by(user_id: :user_id)
-        w.saldo = :monto + w.saldo
+    def cargar_saldo 
+        @wallet = Wallet.find_by(user_id: :user_id)
+        @wallet.saldo = @wallet.saldo + params[:saldo].to_i
+        @wallet.save
+        redirect_to mostrar_saldo_path
+    end
+
+    def wallet_params
+        params.require(:wallet).permit(:user_id, :saldo)
     end
 
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,4 @@
 class Wallet < ApplicationRecord
     belongs_to :user
+    validate :user_id, presence: true
 end
