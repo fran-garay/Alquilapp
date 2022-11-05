39a72b91600d5e69b9e6e2bfdb6ef74588f221e3
Laureano Bruno <laureanobruno@hotmail.com>
2022-11-05 13:04:33 -0300

    Vista de wallet

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -6,13 +6,13 @@ class WalletsController < ApplicationController
 
     def cargar_saldo 
         @wallet = Wallet.find_by(user_id: params[:user_id])
-        @wallet.saldo = @wallet.saldo + params[:saldo].to_i
+        @wallet.saldo = @wallet.saldo + params[:monto].to_i
         @wallet.save
         redirect_to mostrar_saldo_path
     end
 
     def wallet_params
-        params.require(:wallet).permit(:user_id)
+        params.require(:wallet).permit(:user_id, :monto)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
   root "main#home"
 
   get '/wallets/:user_id', :to => 'wallets#mostrar_wallet'
-
+  
 
 
 
