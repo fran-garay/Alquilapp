de4ea91eafb82053149d6fd2868c1f212f5bcbf6
Jero Lambre <jerolambre@gmail.com>
2022-11-26 20:31:51 -0300

    Footer oculto y mejorado

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.hosts << "d12d-163-10-141-224.sa.ngrok.io"
+  config.hosts << "bc71-181-169-216-2.sa.ngrok.io"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
