6bb11b1287dc43f6a01cb5e8e102b1e16dfc7507
fran <franciscogaray@live.com.ar>
2022-11-27 21:24:28 -0300

    alquilar auto y sacar saldo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,8 @@ class UsersController < ApplicationController
     end
     @alquiler = Alquiler.find(current_user.alquiler_id)
     @auto = Auto.find(@alquiler.auto_id)
-    @tiempo_restante = @alquiler.hora_devolucion - Time.now
-    # change tiempo_restante to format hh:mm:ss
-    @tiempo_restante = Time.at(@tiempo_restante).utc.strftime("%H:%M:%S")
-    @tiempo_fin = @alquiler.hora_devolucion - 3.hours
+
+    @tiempo_fin = @alquiler.hora_devolucion
     @tiempo_fin = Time.at(@tiempo_fin).utc.strftime("%H:%M:%S")
 
     # split @tiempo_fin to get hour, minutes and seconds
@@ -37,8 +35,30 @@ class UsersController < ApplicationController
     parametros = alquiler_params
     @user = current_user
     @auto = Auto.find(params[:auto_id])
-    total_horas = (parametros[:fecha_devolucion].to_d - parametros[:fecha_alquiler].to_d).to_i * 24
+    # logger.debug "FECHA #{parametros[:fecha_devolucion]}"
+    # logger.debug "HORA #{parametros[:hora_devolucion]}"
+    # logger.debug "EL PARSE  #{(Date.parse(parametros[:fecha_devolucion]).to_time- Date.today.to_time) / 1.hours}"
+    # todays date
+
+    difference_of_hours_between_today_and_date_value = ((Date.parse(parametros[:fecha_devolucion]).to_time- Date.today.to_time) / 1.hours).to_i
+
+    # console.
+    hora_devolucion = parametros[:hora_devolucion].split(":")[0].to_i
+    minutos_devolucion = parametros[:hora_devolucion].split(":")[1].to_i
+    # get current hour
+    hora_alquiler = Time.now.getlocal.hour.to_i
+    minutos_alquiler = Time.now.getlocal.min.to_i
+
+    horas = hora_devolucion - hora_alquiler
+    minutos = (minutos_devolucion/60 - minutos_alquiler/60).ceil
+    total_horas = horas + minutos + difference_of_hours_between_today_and_date_value
+
+    logger.debug "DEBUGGGGG AHORA ESTE #{total_horas}"
+    # round number to higher integer
+
+    logger.debug "DEBUGGGGG total_horas #{total_horas}"
     total_precio = total_horas * Precio.last.valor
+    @alquiler = Alquiler.new
     if no_tiene_saldo?(total_precio)
       @precio = Precio.last
       @user.errors.add(:base, "No tienes suficiente dinero para alquilar este auto")
@@ -49,11 +69,11 @@ class UsersController < ApplicationController
       @user.errors.add(:base, "La hora de devolución debe ser mayor a la hora actual")
       render "/users/vehiculo" and return
     end
-    @alquiler = Alquiler.new
+
     @alquiler.user_id = current_user.id
     @alquiler.auto_id = params[:auto_id]
     @alquiler.fecha_alquiler = Date.today
-    @alquiler.fecha_alquiler = Time.now
+    @alquiler.hora_alquiler = Time.now
     @alquiler.fecha_devolucion = parametros[:fecha_devolucion]
     @alquiler.hora_devolucion = parametros[:hora_devolucion]
     @alquiler.save
@@ -120,11 +140,15 @@ class UsersController < ApplicationController
     logger.debug "DEBUGS "
     logger.debug "fecha_devolucion #{fecha_devolucion}"
     logger.debug "fecha_devolucion.to_date #{fecha_devolucion.to_date}"
+    logger.debug "hora_devolucion #{hora_devolucion}"
     logger.debug "Date.today #{Date.today.to_s}"
     logger.debug "Date.today #{Date.today.to_s == fecha_devolucion}"
-    logger.debug "Time.now #{Time.now > hora_devolucion}"
+    logger.debug "Time.now #{Time.now.getlocal }"
+    logger.debug "Time.now #{Time.parse(hora_devolucion) - Time.now}"
     # Date.today.
-    fecha_devolucion == Date.today.to_s && hora_devolucion < Time.now
+    # Time now based on the time zone of the server
+
+    fecha_devolucion == Date.today.to_s && Time.parse(hora_devolucion)  < Time.now.getlocal
   end
 
 end
