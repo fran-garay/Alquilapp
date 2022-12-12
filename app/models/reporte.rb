b213c8bda6f15758b18147c8f9989d47dfc51829
Jero Lambre <jerolambre@gmail.com>
2022-12-11 23:30:38 -0300

    Reporte model mejorado

--- app/models/reporte.rb
+++ app/models/reporte.rb
@@ -3,9 +3,9 @@ class Reporte < ApplicationRecord
     has_one :user
     has_one :admin
     validates :descripcion, presence: true
-    validates :id_usuario, presence: true
+    validates :user_id, presence: true
     validates :tipo, presence: true
-    validates :id_alquiler, presence: true
+    validates :alquiler_id, presence: true
     validates :title, presence: true
     enum tipo: [:Estado, :Siniestro, :Pagos]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_11_233533) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_005631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,10 +123,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_11_233533) do
   end
 
   create_table "reportes", force: :cascade do |t|
-    t.integer "id_usuario"
-    t.integer "id_alquiler"
+    t.integer "user_id"
+    t.integer "alquiler_id"
     t.string "descripcion"
-    t.integer "id_supervisor"
+    t.integer "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "tipo", default: 0
